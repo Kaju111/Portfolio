4cc23a2cab23ace4a38ac0bb624fe67492cdc0b9
kaju111 <saikiakaju275@gmail.com>
2024-02-16 12:07:32 +0530

    backend

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,46 +1,47 @@
-import React, { useState } from 'react'
-import {motion} from "framer-motion"
-import About from './About'
-import Portfolio from './Portfolio'
-import Footer from './Footer'
-
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import About from "./About";
+import Portfolio from "./Portfolio";
+import Footer from "./Footer";
 
 const linkOptions = {
-  initial:{
-    scale:0.1,
-    opacity: 0
+  initial: {
+    scale: 0.1,
+    opacity: 0,
   },
-  whileInView:{
+  whileInView: {
     scale: 1,
-    opacity: 1
+    opacity: 1,
   },
-  Transition:{
+  Transition: {
     delay: 1,
-  }}  
+  },
+};
 
 const Intro = () => {
   return (
     <div>
-      
-          <motion.ul {...linkOptions}>
-
-        <div className='text-white h-screen mt-[180px] flex flex-col'>
-          <span className='font-section sm:leading-[80px] md:leading-[140px] leading-[80px] sm:text-[30px] md:text-[40px] text-[30px]'>Hi, my name is...</span>
-          <span className='font-name sm:text-[140px] md:text-[200px] text-[80px]'>
-            <h1 className='md:leading-[150px] leading-[60px] sm:leading-[110px]'>KAJU</h1>
-            <h1 className='md:leading-[150px] leading-[60px] sm:leading-[110px]'>SAIKIA</h1>
-            </span>
+      <motion.ul {...linkOptions}>
+        <div className="text-white h-screen mt-[180px] flex flex-col">
+          <span className="font-section sm:leading-[80px] md:leading-[140px] leading-[80px] sm:text-[30px] md:text-[40px] text-[30px]">
+            Hi,
+          </span>
+          <span className="font-name sm:text-[140px] md:text-[200px] text-[80px]">
+            <h1 className="md:leading-[150px] leading-[60px] sm:leading-[110px]">
+              KAJU
+            </h1>
+            <h1 className="md:leading-[150px] leading-[60px] sm:leading-[110px]">
+              SAIKIA
+            </h1>
+          </span>
         </div>
+      </motion.ul>
 
-          </motion.ul>
-
-      <About/>
-      <Portfolio/>
-      <Footer/>
+      <About />
+      <Portfolio />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Intro
-
-
+export default Intro;
